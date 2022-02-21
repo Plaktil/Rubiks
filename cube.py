f346d2e4611ff47d247f5219e698020acd8df2c3
Plaktil <samuel.lalumiere-lavoie@outlook.com>
2022-02-21 00:19:21 -0500

    Added Rotations
    Completed the function responsible for the rotations

--- cube.py
+++ cube.py
@@ -15,19 +15,31 @@ import numpy as np
 # Create cube class	
 class Cube:
 	# Rotation matrixes
-	_yrot=np.array([
-	[0, 0, 1],
-	[0, 1, 0],
-	[-1, 0, 0]])
-
 	_xrot=np.array([
 	[1, 0, 0],
 	[0, 0, -1],
 	[0, 1, 0]])
+	_inv_xrot=np.array([
+	[1, 0, 0],
+	[0, 0, 1],
+	[0, -1, 0]])
+	
+	_yrot=np.array([
+	[0, 0, 1],
+	[0, 1, 0],
+	[-1, 0, 0]])
+	_inv_yrot=np.array([
+	[0, 0, -1],
+	[0, 1, 0],
+	[1, 0, 0]])
 
 	_zrot=np.array([
 	[0, -1, 0],
 	[1, 0, 0],
+	[0, 0, 1]])
+	_inv_zrot=np.array([
+	[0, 1, 0],
+	[-1, 0, 0],
 	[0, 0, 1]])
 
 	def __init__(self,size):
@@ -47,16 +59,58 @@ class Cube:
 # Block is an array containing the position of the block and a vector containing parameters 
 # representing the orientation of the colored face according to the coordinate and an encoding of its color as its norm
 				for x in range(size):
-					b = Block(z - ((size - 1) / 2), y - ((size - 1) / 2), x - ((size - 1) / 2), size)
+					b = Block(x - ((size - 1) / 2), y - ((size - 1) / 2), z - ((size - 1) / 2), size)
 					cube.append(b)
 			
 			self.cube = cube
 
-	def left(self):
+	def rotation(self, move):
+		rotation_mat = np.array([])
+		side = 0
+		if move == "L":
+			side = self.MIN
+			rotation_mat = self._xrot
+		elif move == "L'":
+			side = self.MIN
+			rotation_mat = self._inv_xrot
+		elif move == "R":
+			side = self.MAX
+			rotation_mat = self._inv_xrot
+		elif move == "R'":
+			side = self.MAX
+			rotation_mat = self._xrot
+		elif move == "U":
+			side = self.MIN
+			rotation_mat = self._yrot
+		elif move == "U'":
+			side = self.MIN
+			rotation_mat = self._inv_yrot
+		elif move == "D":
+			side = self.MAX
+			rotation_mat = self._inv_yrot
+		elif move == "D'":
+			side = self.MAX
+			rotation_mat = self._zrot
+		elif move == "B":
+			side = self.MIN
+			rotation_mat = self._zrot
+		elif move == "B'":
+			side = self.MIN
+			rotation_mat = self._inv_zrot
+		elif move == "F":
+			side = self.MAX
+			rotation_mat = self._inv_zrot
+		elif move == "F'":
+			side = self.MAX
+			rotation_mat = self._zrot
+
 		for b in self.cube:
 			if b.x == self.MIN:
+				print(b.pos, b.colors)
 				b.pos = np.matmul(b.pos, self._xrot)
-
+				b.colors = np.matmul(b.colors, self._xrot)
+				print(b.pos, b.colors)
+				print("-=-=-=-=-=-=")
 
 class Block():
 	def __init__(self, x, y ,z, size):
@@ -66,18 +120,20 @@ class Block():
 		self.y = self.pos[1]
 		self.z = self.pos[2]
 
-		if x==0:
+		max_coord = ((size - 1) / 2)
+
+		if x == -max_coord:
 			self.colors[0] = -5
-		if x==size - 1:
+		if x == max_coord:
 			self.colors[0] = 2
-		if y==0:
-			self.colors[1] = 1
-		if y==size - 1:
-			self.colors[1] = -6
-		if z==0:
-			self.colors[2] = 3
-		if z==size - 1:
-			self.colors[2] = -4
+		if y == -max_coord:
+			self.colors[1] = -1
+		if y == max_coord:
+			self.colors[1] = 6
+		if z == -max_coord:
+			self.colors[2] = -3
+		if z == max_coord- 1:
+			self.colors[2] = 4
 
 	def draw():
 		pass
@@ -88,6 +144,4 @@ a=Cube(3)
 for b in a.cube: 
 	print(b.pos, b.colors)
 
-a.left()
-for b in a.cube: 
-	print(b.pos, b.colors)
+
--- main.py
+++ main.py
@@ -1,13 +1,1 @@
 import numpy as np
-
-def main():
-    x = np.array([[-1, 0, 0],
-                 [0, 1, 0],
-                 [0, 0, 1]])
-
-    y = np.array([-1, 1, 1])
-
-    z = np.matmul(y, x)
-    print(z)
-
-main()
